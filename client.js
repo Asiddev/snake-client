ed3e55d741ad7ac55839c2c91749d1d46dd9f93b
Alexander Sidorenko <asiddev@protonmail.com>
2022-09-26 21:30:29 -0700

    added my name to the board

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const myName = "ALX";
 
 // establishes a connection with the game server
 const connect = function () {
@@ -11,12 +12,13 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
-    console.log("Its Alex!");
+    console.log("Successfully connected to game server");
+    conn.write(`Name: ${myName}`);
   });
 
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
+  // conn.on("data", (myName) => {
+  //   console.log("Server says: ", myName);
+  // });
 
   return conn;
 };
